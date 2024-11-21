9a0762d8f92fae59832a39a89d5868ed91226066
Mark Guyada <markguyadaez@gmail.com>
2024-11-22 01:34:59 +0800

    Just checking

--- backend/src/utils/generateToken.ts
+++ backend/src/utils/generateToken.ts
@@ -8,7 +8,7 @@ export const generateToken = (res: Response, userId: unknown): void => {
 
   res.cookie("token", token, {
     httpOnly: true,
-    secure: process.env.NODE_ENV !== "development",
+    // secure: process.env.NODE_ENV !== "development",
     sameSite: "strict",
     maxAge: 30 * 24 * 60 * 60 * 1000,
   });
