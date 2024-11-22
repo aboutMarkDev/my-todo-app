2d625fe4a1d2541f2c7ef38528cf203d69a30f39
Mark <chaseaxieez@gmail.com>
2024-11-22 21:21:13 +0800

    Add options in cookie for logging out user

--- backend/src/controllers/users.ts
+++ backend/src/controllers/users.ts
@@ -48,7 +48,11 @@ export const loginUser = asyncHandler(
 );
 
 export const logoutUser = (req: Request, res: Response) => {
-  res.clearCookie("token");
+  res.clearCookie("token", {
+    httpOnly: true,
+    secure: process.env.NODE_ENV !== "development",
+    sameSite: "none",
+  });
   res.status(200).json({ message: "Logout user" });
 };
 
