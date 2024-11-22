2ddb8b3a6f882bfc6b97309fec3f73a79d14abde
Mark Guyada <markguyadaez@gmail.com>
2024-11-22 15:27:40 +0800

    Change same site to none if the app is in production?

--- backend/src/utils/generateToken.ts
+++ backend/src/utils/generateToken.ts
@@ -8,9 +8,8 @@ export const generateToken = (res: Response, userId: unknown): void => {
 
   res.cookie("token", token, {
     httpOnly: true,
-    // secure: process.env.NODE_ENV !== "development",
-    secure: false,
-    sameSite: "strict",
+    secure: process.env.NODE_ENV !== "development",
+    sameSite: "none",
     maxAge: 30 * 24 * 60 * 60 * 1000,
   });
 };
